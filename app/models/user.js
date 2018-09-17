c81b20262932390070f9ede1f6ea5946dbc9da6c
Jeff Chasin <jeff@jeffchasin.com>
2018-09-17 10:28:10 -0700

    [WIP] User model set up to hash password

--- app/models/user.js
+++ app/models/user.js
@@ -6,7 +6,17 @@ var Promise = require('bluebird');
 // var hash = bcrypt.hashSync(password, salt);
 
 var User = db.Model.extend({
-  tableName: 'users'
+  tableName: 'users',
+
+  initialize: function() {
+   
+    this.on('creating', function(model, attrs, options) {
+      
+      var salt = bcrypt.genSaltSync(10);
+      var hash = bcrypt.hashSync(model.get('password'), salt);
+      model.set('password', hash);
+    });
+  }
 });
 
 module.exports = User;
